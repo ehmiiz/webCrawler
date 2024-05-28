8da86cd2adcd2e8635df6d99b2796292df96141f
emil <emil@wirely.se>
2024-05-28 09:37:35 +0200

    getURLsFromHTML function & tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,36 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/'){
+            // relative url
+            try {
+                const urlObject = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObject.href)
+            }
+            catch (err) {
+                console.log(`error with relative url: ${err.message}`)
+            } 
+        }
+        else {
+            // absolute
+            try {
+                const urlObject = new URL(linkElement.href)
+                urls.push(urlObject.href)
+            }
+            catch (err) {
+                console.log(`error with absolute url: ${err.message}`)
+            } 
+        }
+    }
+
+    return urls
+}
+
 function normalizeURL (urlString) {
 
     const urlObject = new URL(urlString)
@@ -11,5 +44,6 @@ function normalizeURL (urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
